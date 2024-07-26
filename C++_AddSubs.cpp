dd7bd8d4fd83fec4f3d18f0a442df9585c31cbe9
Hyper7711 <143863603+Hyper7711@users.noreply.github.com>
2024-07-26 18:59:52 +0530

    c++_basics

---
+++ C++_AddSubs.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+struct Calculator {
+    enum Operation { ADDITION, SUBTRACTION };
+    static int count;
+    static void incrementCount() { count++; }
+};
+
+int Calculator::count = 0;
+
+int add(int a, int b) {
+    Calculator::incrementCount();
+    return a + b;
+}
+ṇṇ
+int subtract(int a, int b) {
+    Calculator::incrementCount();
+    return a - b;
+}
+
+int main() {
+    int num1, num2, choice;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Choose operation:\n";
+    cout << "1. Addition\n";
+    cout << "2. Subtraction\n";
+    cin >> choice;
+
+    int result;
+    switch (choice) {
+        case 1:
+            result = add(num1, num2);
+            cout << "Result of addition: " << result << endl;
+            break;
+        case 2:
+            result = subtract(num1, num2);
+            cout << "Result of subtraction: " << result << endl;
+            break;
+        default:
+            cout << "Invalid choice" << endl;
+            break;
+    }
+
+    cout << "Total operations performed :" << Calculator::count << endl;
+
+    return 0;
+}       
+
+
